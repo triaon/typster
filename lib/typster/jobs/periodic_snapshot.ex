4fa124e81011286ef43f60e10a4068ff9f924606
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2026-04-24 14:23:22 +0300

    chore: address credo follow-ups

--- lib/typster/jobs/periodic_snapshot.ex
+++ lib/typster/jobs/periodic_snapshot.ex
@@ -2,37 +2,40 @@ defmodule Typster.Jobs.PeriodicSnapshot do
   @moduledoc "Oban job that snapshots file contents into file revisions"
   use Oban.Worker, queue: :default, max_attempts: 3
   import Ecto.Query
+  alias Typster.Projects.FileRevision
+  alias Typster.Repo
 
   @impl Oban.Worker
   def perform(_job) do
-    alias Typster.Projects.FileRevision
-    alias Typster.Repo
-
     files = Typster.Repo.all(Typster.Projects.File)
 
     Enum.each(files, fn file ->
-      revisions =
-        from(r in FileRevision, where: r.file_id == ^file.id, order_by: [desc: r.sequence])
-        |> Repo.all()
+      latest_revision =
+        from(r in FileRevision,
+          where: r.file_id == ^file.id,
+          order_by: [desc: r.sequence],
+          limit: 1
+        )
+        |> Repo.one()
 
-      case revisions do
-        [latest_revision | _rest] ->
-          if latest_revision.content != file.content do
-            sequence = latest_revision.sequence + 1
-
-            %FileRevision{file_id: file.id, inserted_at: DateTime.utc_now(:second)}
-            |> FileRevision.changeset(%{
-              content: file.content,
-              sequence: sequence
-            })
-            |> Repo.insert()
-          end
-
-        [] ->
-          :ok
-      end
+      maybe_insert_revision(file, latest_revision)
     end)
 
     :ok
+  end
+
+  defp maybe_insert_revision(_file, nil), do: :ok
+
+  defp maybe_insert_revision(file, latest_revision) do
+    if latest_revision.content != file.content do
+      sequence = latest_revision.sequence + 1
+
+      %FileRevision{file_id: file.id, inserted_at: DateTime.utc_now(:second)}
+      |> FileRevision.changeset(%{
+        content: file.content,
+        sequence: sequence
+      })
+      |> Repo.insert()
+    end
   end
 end
